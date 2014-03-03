0f0f29b6e4c3396833ee32ab711fe7ad899d58a5
Adam Lundrigan <adam@lundrigan.ca>
2014-03-02 20:50:22 -0330

    Install ZfcUser (my fork) and ZfcUserDoctrineORM

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,14 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
+        'ZfcBase',
+        'DoctrineModule',
+        'DoctrineORMModule',
+        'ZfcUser',
+        'ZfcUserDoctrineORM',
+
         'UsaRugbyStats\Application',
+        'UsaRugbyStats\Account',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
