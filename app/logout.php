00e5415d230e191fe0e7be43f3ab7702006bb715
Adam Lundrigan <adam@lundrigan.ca>
2014-03-04 23:20:09 -0330

    Remove legacy User Account scripts from /app

--- app/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once './session.php';
-$_SESSION = array(); // destroy all $_SESSION data
-setcookie(session_name(), "", time() - 3600, "/");
-session_destroy();
-header('Location: http://' . $_SERVER['HTTP_HOST'] . '/login.php');
