48a50db3fa1de74dda6dd95e69f1b70b6d1c0b63
Adam Lundrigan <adam@lundrigan.ca>
2014-03-09 12:50:24 -0230

    Add Doctrine class table inheritance on RoleAssignment (allows specifying extra data on each user+role combination)

---
+++ module/UsaRugbyStats/Account/src/Entity/Rbac/RoleAssignment/Member.php
@@ -0,0 +1,8 @@
+<?php
+namespace UsaRugbyStats\Account\Entity\Rbac\RoleAssignment;
+
+use UsaRugbyStats\Account\Entity\Rbac\RoleAssignment as BaseAssignment;
+
+class Member extends BaseAssignment
+{
+}
