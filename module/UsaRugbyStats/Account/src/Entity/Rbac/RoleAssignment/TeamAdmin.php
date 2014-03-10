edb2ef2283d612116503b8b16d1d5c6e6a8599ae
Adam Lundrigan <adam@lundrigan.ca>
2014-03-09 22:43:45 -0230

    Add association between the team_admin role and specific team(s)

--- module/UsaRugbyStats/Account/src/Entity/Rbac/RoleAssignment/TeamAdmin.php
+++ module/UsaRugbyStats/Account/src/Entity/Rbac/RoleAssignment/TeamAdmin.php
@@ -2,7 +2,63 @@
 namespace UsaRugbyStats\Account\Entity\Rbac\RoleAssignment;
 
 use UsaRugbyStats\Account\Entity\Rbac\RoleAssignment as BaseAssignment;
+use UsaRugbyStats\Account\Entity\Team;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class TeamAdmin extends BaseAssignment
 {
+    protected $managedTeams;   
+
+    /**
+     * Init the Doctrine collection
+     */
+    public function __construct()
+    {
+        $this->managedTeams = new ArrayCollection();
+    }
+    
+    /**
+     * @return Collection
+     */
+    public function getManagedTeams()
+    {
+        return $this->managedTeams;
+    }
+    
+    /**
+     * @param Collection $managedTeams
+     * @return self
+     */
+    public function setManagedTeams(Collection $managedTeams)
+    {
+        $this->managedTeams = new ArrayCollection();
+        if(count($managedTeams)){
+            foreach($managedTeams as $t) {
+                $this->addManagedTeam($t);
+            }
+        }
+        return $this;
+    }
+    
+    /**
+     * @param ManagedTeam $t
+     * @return self
+     */
+    public function addManagedTeam(Team $t)
+    {
+        $this->managedTeams->add($t);
+        return $this;
+    }
+    
+    /**
+     * @param ManagedTeam $t
+     * @return bool
+     */
+    public function hasManagedTeam(Team $t)
+    {
+        return $this->managedTeams->contains($t);
+    }
+
+    
 }
