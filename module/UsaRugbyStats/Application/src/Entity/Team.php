a9704b61fcd44943cf48b0085337cea52896768b
Adam Lundrigan <adam@lundrigan.ca>
2014-03-09 23:22:36 -0230

    Add getters and setters to Team entity

--- module/UsaRugbyStats/Application/src/Entity/Team.php
+++ module/UsaRugbyStats/Application/src/Entity/Team.php
@@ -62,4 +62,125 @@ class Team
      * @var string
      */
     protected $groupAboveUuid;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getHidden()
+    {
+        return $this->hidden;
+    }
+
+    public function setHidden($hidden)
+    {
+        $this->hidden = $hidden;
+    }
+
+    public function getUserCreate()
+    {
+        return $this->userCreate;
+    }
+
+    public function setUserCreate($userCreate)
+    {
+        $this->userCreate = $userCreate;
+    }
+
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getShort()
+    {
+        return $this->short;
+    }
+
+    public function setShort($short)
+    {
+        $this->short = $short;
+    }
+
+    public function getResources()
+    {
+        return $this->resources;
+    }
+
+    public function setResources($resources)
+    {
+        $this->resources = $resources;
+    }
+
+    public function getLogoUrl()
+    {
+        return $this->logoUrl;
+    }
+
+    public function setLogoUrl($logoUrl)
+    {
+        $this->logoUrl = $logoUrl;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    public function getGroupAboveUuid()
+    {
+        return $this->groupAboveUuid;
+    }
+
+    public function setGroupAboveUuid($groupAboveUuid)
+    {
+        $this->groupAboveUuid = $groupAboveUuid;
+    }
+
 }
