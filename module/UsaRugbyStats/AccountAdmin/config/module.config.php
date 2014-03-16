ac42fd68ab23933d7e27bff58669e5a79e89dabe
Adam Lundrigan <adam@lundrigan.ca>
2014-03-15 22:36:36 -0230

    Add module AccountAdmin where extensions to ZfcUserAdmin will live

---
+++ module/UsaRugbyStats/AccountAdmin/config/module.config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'view_manager' => array(
+        'template_map' => array(
+            'zfc-user-admin/user-admin/create'  => __DIR__ . '/../view/zfc-user-admin/user-admin/create.phtml',
+            'zfc-user-admin/user-admin/edit'    => __DIR__ . '/../view/zfc-user-admin/user-admin/edit.phtml',
+        ),
+    ),
+);
