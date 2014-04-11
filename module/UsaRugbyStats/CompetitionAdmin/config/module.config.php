7a9c82dcf3c7d952ca67424b91abb87a6db3f5d2
Adam Lundrigan <adam@lundrigan.ca>
2014-04-10 23:01:15 -0230

    Add skeleton of CRUD panel for Competition domain entity

--- module/UsaRugbyStats/CompetitionAdmin/config/module.config.php
+++ module/UsaRugbyStats/CompetitionAdmin/config/module.config.php
@@ -107,6 +107,57 @@ return array(
                             ),
                         ),
                     ),
+                    'usarugbystats_competitionadmin' => array(
+                        'type' => 'Literal',
+                        'priority' => 1000,
+                        'options' => array(
+                            'route' => '/competition',
+                            'defaults' => array(
+                                'controller' => 'usarugbystats_competitionadmin_controller',
+                                'action'     => 'index',
+                            ),
+                        ),
+                        'child_routes' =>array(
+                            'list' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/list',
+                                    'defaults' => array(
+                                        'action'     => 'list',
+                                    ),
+                                ),
+                            ),
+                            'create' => array(
+                                'type' => 'Literal',
+                                'options' => array(
+                                    'route' => '/create',
+                                    'defaults' => array(
+                                        'action'     => 'create'
+                                    ),
+                                ),
+                            ),
+                            'edit' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/edit/:id',
+                                    'defaults' => array(
+                                        'action'     => 'edit',
+                                        'userId'     => 0
+                                    ),
+                                ),
+                            ),
+                            'remove' => array(
+                                'type' => 'Segment',
+                                'options' => array(
+                                    'route' => '/remove/:id',
+                                    'defaults' => array(
+                                        'action'     => 'remove',
+                                        'userId'     => 0
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
                 ),
             ),
         ),
@@ -115,6 +166,7 @@ return array(
         'invokables' => array(
             'usarugbystats_teamadmin_controller' => 'UsaRugbyStats\CompetitionAdmin\Controller\TeamAdminController',
             'usarugbystats_unionadmin_controller' => 'UsaRugbyStats\CompetitionAdmin\Controller\UnionAdminController',
+            'usarugbystats_competitionadmin_controller' => 'UsaRugbyStats\CompetitionAdmin\Controller\CompetitionAdminController',
         ),
     ),    
     'navigation' => array(
@@ -136,6 +188,16 @@ return array(
                     'create' => array(
                         'label' => 'New Union',
                         'route' => 'zfcadmin/usarugbystats_unionadmin/create',
+                    ),
+                ),
+            ),
+            'usarugbystats_competitionadmin' => array(
+                'label' => 'Competitions',
+                'route' => 'zfcadmin/usarugbystats_competitionadmin/list',
+                'pages' => array(
+                    'create' => array(
+                        'label' => 'New Competition',
+                        'route' => 'zfcadmin/usarugbystats_competitionadmin/create',
                     ),
                 ),
             ),
