12e907340545bce5b518255228001b573158c3b5
Adam Lundrigan <adam@lundrigan.ca>
2014-04-17 00:52:24 -0230

    Integrate Behat + Mink + Selenium Web Driver

---
+++ .travisci/start_selenium2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -e
+
+# Make sure that Xvfb starts everytime the box/vm is booted:
+echo "Starting X virtual framebuffer (Xvfb) in background..."
+Xvfb -ac :99 -screen 0 1280x1024x16 &
+export DISPLAY=:99
+
+curl http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar > /tmp/selenium.jar
+java -jar /tmp/selenium.jar > /dev/null 2> /tmp/webdriver_output.txt &
