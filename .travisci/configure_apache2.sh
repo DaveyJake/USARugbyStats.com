d5f7d20f96713a3c221ca74419db41be76e1c8d4
Adam Lundrigan <adam@lundrigan.ca>
2014-04-18 16:35:33 -0230

    Move Apache2 configuration into bash script

---
+++ .travisci/configure_apache2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install apache2 libapache2-mod-fastcgi 
+sudo a2enmod rewrite actions fastcgi alias
+
+[ "$(phpenv version-name)" = "hhvm" ]  && .travisci/hhvm/configure.sh
+[ "$(phpenv version-name)" != "hhvm" ] && .travisci/php/configure.sh
+
