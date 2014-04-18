75c892f52d3c731cd0535aaf30749e6cf2b07b7a
Adam Lundrigan <adam@lundrigan.ca>
2014-04-18 19:45:44 -0230

    [Travis] try some more random stuff

--- .travisci/hhvm/configure.sh
+++ .travisci/hhvm/configure.sh
@@ -3,10 +3,10 @@
 # configure apache virtual hosts
 sudo cp -f .travisci/hhvm/apache22_vhost.txt /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+sudo rm /etc/apache2/mods-available/hhvm_proxy_fcgi.conf
 
 # restart everything
-sudo service hhvm stop 
 sudo service apache2 restart
 
 # run HHVM in FastCGI mode
-hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9000 &
+sudo service hhvm restart 
