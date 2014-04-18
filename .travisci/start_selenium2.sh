d4a545cc4e5398041bc507cacd310c7aaab211fd
Adam Lundrigan <adam@lundrigan.ca>
2014-04-18 12:51:56 -0230

    Send the output of Xvfb to file so it doesn't pollute the test output

--- .travisci/start_selenium2.sh
+++ .travisci/start_selenium2.sh
@@ -3,7 +3,7 @@ set -e
 
 # Make sure that Xvfb starts everytime the box/vm is booted:
 echo "Starting X virtual framebuffer (Xvfb) in background..."
-Xvfb -ac :99 -screen 0 1280x1024x16 &
+Xvfb -ac :99 -screen 0 1280x1024x16 > /tmp/xvfb_output.txt 2>&1 &
 export DISPLAY=:99
 
 curl http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar > /tmp/selenium.jar
