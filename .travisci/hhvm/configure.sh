9e2e5673b5d88cb56573e16db81329da3b99185e
Adam Lundrigan <adam@lundrigan.ca>
2014-04-18 18:25:57 -0230

    [Travis] Remove install_fastcgi.sh call

--- .travisci/hhvm/configure.sh
+++ .travisci/hhvm/configure.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 sudo apt-get install -y --force-yes hhvm-fastcgi
-sudo /usr/share/hhvm/install_fastcgi.sh
-sudo service hhvm restart
 
 # configure apache virtual hosts
 sudo cp -f .travisci/hhvm/apache22_vhost.txt /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
+sudo service hhvm restart
 sudo service apache2 restart
 
