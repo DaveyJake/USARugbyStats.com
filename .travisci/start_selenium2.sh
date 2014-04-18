169c0b389936d8db2081655d54a80655fd67961f
Adam Lundrigan <adam@lundrigan.ca>
2014-04-18 12:47:21 -0230

    Build in a delay after starting Selenium

--- .travisci/start_selenium2.sh
+++ .travisci/start_selenium2.sh
@@ -8,3 +8,5 @@ export DISPLAY=:99
 
 curl http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar > /tmp/selenium.jar
 java -jar /tmp/selenium.jar > /dev/null 2> /tmp/webdriver_output.txt &
+
+sleep 5
