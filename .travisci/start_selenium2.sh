dce183eec40123b6bd5ca493ad2e5892ddfb7aad
Adam Lundrigan <adam@lundrigan.ca>
2014-04-18 13:25:18 -0230

    Yet another attempt to get Behat+Mink+Selenium to pass on Travis

--- .travisci/start_selenium2.sh
+++ .travisci/start_selenium2.sh
@@ -3,8 +3,10 @@ set -e
 
 # Make sure that Xvfb starts everytime the box/vm is booted:
 echo "Starting X virtual framebuffer (Xvfb) in background..."
-Xvfb -ac :99 -screen 0 1280x1024x16 > /tmp/xvfb_output.txt 2>&1 &
-export DISPLAY=:99
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+
+sleep 5
 
 curl http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar > /tmp/selenium.jar
 java -jar /tmp/selenium.jar > /dev/null 2> /tmp/webdriver_output.txt &
