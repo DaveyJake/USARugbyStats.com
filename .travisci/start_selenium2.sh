80a52c0e4270840b58b615c62d91c9c252494b78
Adam Lundrigan <adam@lundrigan.ca>
2014-04-20 21:54:03 -0230

    Add an interactive mode to start_selenium2.sh

--- .travisci/start_selenium2.sh
+++ .travisci/start_selenium2.sh
@@ -10,5 +10,15 @@ sleep 5
 
 curl http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar > /tmp/selenium.jar
 java -jar /tmp/selenium.jar > /dev/null 2> /tmp/webdriver_output.txt &
+SELENIUMPID=$!
 
 sleep 5
+
+if [ "$1" = "interactive" ];
+then
+    echo -e "\nPress and key to terminate Selenium and Xvfb...";
+    read QUIT
+
+    kill -9 $SELENIUMPID
+    sh -e /etc/init.d/xvfb stop
+fi;
