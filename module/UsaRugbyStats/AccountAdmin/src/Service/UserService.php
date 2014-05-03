c16f20a37697c9b1e9df0967235c439ce5e872b5
Adam Lundrigan <adam@lundrigan.ca>
2014-05-03 14:31:47 -0230

    Update AccountAdmin UserService to hack around #15

--- module/UsaRugbyStats/AccountAdmin/src/Service/UserService.php
+++ module/UsaRugbyStats/AccountAdmin/src/Service/UserService.php
@@ -6,6 +6,9 @@ use Zend\Form\Form;
 use UsaRugbyStats\Account\Entity\Account as AccountEntity;
 use ZfcUser\Entity\UserInterface;
 use Zend\Filter\Word\CamelCaseToUnderscore;
+use UsaRugbyStats\Account\Entity\Rbac\RoleAssignment\UnionAdmin;
+use UsaRugbyStats\Account\Entity\Rbac\RoleAssignment\CompetitionAdmin;
+use UsaRugbyStats\Account\Entity\Rbac\RoleAssignment\TeamAdmin;
 
 class UserService extends ZfcUserAdminUserService
 {
@@ -20,15 +23,22 @@ class UserService extends ZfcUserAdminUserService
 
     public function edit(Form $form, array $data, UserInterface $user)
     {
-        $this->populateRoleAssignmentInputDataWithEntityClassNames($data);
+        $this->populateRoleAssignmentInputDataWithEntityClassNames($data, $user);
 
         return parent::edit($form, $data, $user);
     }
 
-    protected function populateRoleAssignmentInputDataWithEntityClassNames(&$data)
+    protected function populateRoleAssignmentInputDataWithEntityClassNames(&$data, UserInterface $user = NULL)
     {
         if ( ! isset($data['roleAssignments']) || count($data['roleAssignments']) == 0 ) {
+            // @HACK to fix GH-15 (Can't empty an existing Collection)
+            if ($user instanceof AccountEntity) {
+                $user->removeRoleAssignments($user->getRoleAssignments());
+            }
+
             $data['roleAssignments'] = array();
+
+            return;
         }
 
         $filterCamelToUnderscore = new CamelCaseToUnderscore();
@@ -42,6 +52,43 @@ class UserService extends ZfcUserAdminUserService
                 unset($data['roleAssignments'][$k]);
                 continue;
             }
+
+            // @HACK to fix GH-15 (Can't empty an existing Collection)
+            if ( $user instanceof AccountEntity ) {
+                switch ($key) {
+                    case 'competition_admin':
+                    {
+                        if ( ! isset($data['roleAssignments'][$k]['managedCompetitions']) || count($data['roleAssignments'][$k]['managedCompetitions']) == 0 ) {
+                            $obj = $user->getRoleAssignment($types[$key]['name']);
+                            if ( $obj instanceof CompetitionAdmin ) {
+                                $obj->removeManagedCompetitions($obj->getManagedCompetitions());
+                            }
+                        }
+                        break;
+                    }
+                    case 'team_admin':
+                    {
+                        if ( ! isset($data['roleAssignments'][$k]['managedTeams']) || count($data['roleAssignments'][$k]['managedTeams']) == 0 ) {
+                            $obj = $user->getRoleAssignment($types[$key]['name']);
+                            if ( $obj instanceof TeamAdmin ) {
+                                $obj->removeManagedTeams($obj->getManagedTeams());
+                            }
+                        }
+                        break;
+                    }
+                    case 'union_admin':
+                    {
+                        if ( ! isset($data['roleAssignments'][$k]['managedUnions']) || count($data['roleAssignments'][$k]['managedUnions']) == 0 ) {
+                            $obj = $user->getRoleAssignment($types[$key]['name']);
+                            if ( $obj instanceof UnionAdmin ) {
+                                $obj->removeManagedUnions($obj->getManagedUnions());
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+
             $data['roleAssignments'][$k]['__class__'] = $types[$key]['entity_class'];
             unset($data['roleAssignments'][$k]['type']);
         }
