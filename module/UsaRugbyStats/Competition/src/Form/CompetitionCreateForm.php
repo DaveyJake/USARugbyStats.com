8dc2a6d80394660403d8c9c130e6d370c1bf02f8
Adam Lundrigan <adam@lundrigan.ca>
2014-05-03 22:36:29 -0230

    Ensure that two divisions of the same competition can't share a name Closes #34

--- module/UsaRugbyStats/Competition/src/Form/CompetitionCreateForm.php
+++ module/UsaRugbyStats/Competition/src/Form/CompetitionCreateForm.php
@@ -23,4 +23,26 @@ class CompetitionCreateForm extends Form
             ),
         ));
     }
+
+    public function isValid()
+    {
+        $result = parent::isValid();
+
+        $validData = $this->getInputFilter()->getValidInput()['competition'];
+        $fsCompetition = $this->get('competition');
+
+        // Ensure that each division of the competition has a unique name
+        $divisionNames = array();
+        foreach ( $validData->get('divisions')->getValues() as $key => $arrDivision ) {
+            if ( in_array($arrDivision['name'], $divisionNames) ) {
+                $fsCompetition->get('divisions')->get($key)->get('name')->setMessages([
+                    "There is already a division with this name!"
+                ]);
+                $result = false;
+            }
+            array_push($divisionNames, $arrDivision['name']);
+        }
+
+        return $result;
+    }
 }
