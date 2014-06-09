c08c9fd8570f6daf446dd28e2f3663170f53c489
Adam Lundrigan <adam@lundrigan.ca>
2014-06-09 16:02:04 -0230

    Update the redirect parser to work with latest version of ZfcUser

--- module/UsaRugbyStats/Account/Module.php
+++ module/UsaRugbyStats/Account/Module.php
@@ -3,6 +3,8 @@ namespace UsaRugbyStats\Account;
 
 use Zend\Mvc\MvcEvent;
 use Zend\Http\PhpEnvironment\Request;
+use Zend\Mvc\Router\RouteMatch;
+use Zend\Mvc\Router\RouteInterface;
 
 class Module
 {
@@ -30,13 +32,48 @@ class Module
                 return;
             }
 
-            $uri = new \Zend\Uri\Uri($request->getQuery('redirect'));
-            if ( $uri->isAbsolute() ) {
-                $redirect = ($uri->getHost() == $request->getUri()->getHost())
-                    ? $uri->getPath()
-                    : NULL;
-                $request->getQuery()->set('redirect', $redirect);
+            // We only need to do this if the redirect parameter is a valid URI
+            $parameterSource = $request->getPost()->offsetExists('redirect')
+                ? $request->getPost()
+                : $request->getQuery();
+
+            $uri = new \Zend\Uri\Uri($parameterSource->get('redirect'));
+            if ( ! $uri->isValid() ) {
+                return;
             }
+
+            // If it's an absolute URI and not the same as this host, kill it and short-circuit
+            if ( $uri->isAbsolute() && $uri->getHost() != $request->getUri()->getHost() ) {
+                $parameterSource->offsetUnset('redirect');
+                return;
+            }
+
+            // Determine if the paramater passed in a valid route name.  If it is, we're done here...
+            $parts = explode('/', $uri->getPath());
+            $router = $e->getRouter();
+            foreach ( $parts as $part ) {
+                if ( ! $router->hasRoute($part) ) {
+                    $router = NULL;
+                    break;
+                }
+                $router = $router->getRoute($part);
+            }
+            if ( $router instanceof RouteInterface ) {
+                return;
+            }
+
+            // Use the router to turn the relative URI provided into a route name
+            $subreq = new \Zend\Http\Request();
+            $subreq->setUri($uri->toString());
+            $routeMatch = $e->getRouter()->match($subreq);
+
+            // If it matches a route, set that route as the target...otherwise kill it
+            if ( $routeMatch instanceof RouteMatch ) {
+                $parameterSource->set('redirect', $routeMatch->getMatchedRouteName());
+            } else {
+                $parameterSource->offsetUnset('redirect');
+            }
+
         }, 1000);
     }
 
