e568f767ed376d89d6c960db7ac103ec07eefee7
Adam Lundrigan <adam@lundrigan.ca>
2014-07-13 15:29:33 -0230

    Add permissions for competition.team to PermissionsFixture

--- module/UsaRugbyStats/Competition/src/Fixtures/Common/PermissionsFixture.php
+++ module/UsaRugbyStats/Competition/src/Fixtures/Common/PermissionsFixture.php
@@ -49,6 +49,12 @@ class PermissionsFixture implements FixtureInterface, DependentFixtureInterface
     }
 
     protected $permissions = [
+        'competition.team.list' => ['member'],
+        'competition.team.create' => ['super_admin'],
+        'competition.team.update' => ['team_admin'],
+        'competition.team.update.union' => ['super_admin'],
+        'competition.team.delete' => ['super_admin'],
+
         'competition.competition.list' => ['member'],
         'competition.competition.create' => ['super_admin'],
         'competition.competition.update' => ['competition_admin'],
