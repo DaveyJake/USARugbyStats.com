8ececc0c981dd8498a498efdf656fde382f36348
Adam Lundrigan <adam@lundrigan.ca>
2014-07-28 16:44:53 -0230

    Don't skip next test suite on failure

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 
 for DIR in module config; do bin/php-cs-fixer fix -v --fixers=-psr0,psr4 $DIR; done;
 
-find module -type d -name "tests" | while read DIR; do ./bin/phpunit -c $DIR/phpunit.xml || exit 1; done;
+find module -type d -name "tests" | while read DIR; do ./bin/phpunit -c $DIR/phpunit.xml; done;
