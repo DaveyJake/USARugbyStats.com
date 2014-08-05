21b3709269fdca820d7ad00271148c95487e4ef3
Adam Lundrigan <adam@lundrigan.ca>
2014-08-05 12:34:16 -0230

    Add missing field to TestCompetitionFixture

--- module/UsaRugbyStats/Competition/src/Fixtures/Development/TestCompetitionsFixture.php
+++ module/UsaRugbyStats/Competition/src/Fixtures/Development/TestCompetitionsFixture.php
@@ -41,8 +41,8 @@ class TestCompetitionsFixture implements FixtureInterface, DependentFixtureInter
     }
 
     protected $competitionData = array(
-        [ 'name' => 'Test Competition #1', 'variant' => '15s', 'startDate' => '2014-07-17T14:22-06:00', 'endDate' => '2014-07-23T08:09-06:00' ],
-        [ 'name' => 'Test Competition #2', 'variant' => '7s', 'startDate' => '2014-07-08T14:22-06:00', 'endDate' => '2014-07-23T08:09-06:00' ],
+        [ 'name' => 'Test Competition #1', 'variant' => '15s', 'maxPlayersOnRoster' => 0, 'startDate' => '2014-07-17T14:22-06:00', 'endDate' => '2014-07-23T08:09-06:00' ],
+        [ 'name' => 'Test Competition #2', 'variant' => '7s', 'maxPlayersOnRoster' => 12, 'startDate' => '2014-07-08T14:22-06:00', 'endDate' => '2014-07-23T08:09-06:00' ],
     );
 
 }
