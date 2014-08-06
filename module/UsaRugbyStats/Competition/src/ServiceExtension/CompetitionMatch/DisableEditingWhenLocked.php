f1b44b81e76f3721a3e7e8e37be87a1f19077101
Adam Lundrigan <adam@lundrigan.ca>
2014-08-06 02:08:50 -0230

    Fix a few visual niggles with the display of signatures when the match is locked

--- module/UsaRugbyStats/Competition/src/ServiceExtension/CompetitionMatch/DisableEditingWhenLocked.php
+++ module/UsaRugbyStats/Competition/src/ServiceExtension/CompetitionMatch/DisableEditingWhenLocked.php
@@ -14,31 +14,19 @@ class DisableEditingWhenLocked extends AbstractRule
         if ( ! isset($e->getParams()->entity) ) {
             return false;
         }
-        if ( ! isset($e->getParams()->flags) ) {
-            return false;
-        }
 
         return true;
     }
 
     public function execute(EventInterface $e)
     {
-        $isFinished = $e->getParams()->entity->isFinished()
-            && $e->getParams()->entity->hasSignature('HC')
-            && $e->getParams()->entity->hasSignature('AC')
-            && $e->getParams()->entity->hasSignature('REF')
-            && $e->getParams()->entity->hasSignature('NR4');
-
-        if ($isFinished) {
-            $e->getParams()->entity->setIsLocked(true);
-        }
-
         if ( $e->getParams()->entity->isLocked() ) {
             $this->disableDetailsChange($e->getParams());
             $this->disableTeamChange($e->getParams());
             $this->disableTeamEventsChange($e->getParams());
             $this->disableTeamRosterChange($e->getParams());
             $this->disableSignatureChange($e->getParams());
+            $e->getParams()->flags->{'match.signatures.allowed_types'} = [];
         }
     }
 }
