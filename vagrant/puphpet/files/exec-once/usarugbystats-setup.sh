542d37529bcf5acd7061c53bdb152eebf91243d9
Adam Lundrigan <adam@lundrigan.ca>
2014-08-07 16:19:50 -0230

    Update Vagrant configuration (extend Composer timeout window & uncomment a fixture name)

--- vagrant/puphpet/files/exec-once/usarugbystats-setup.sh
+++ vagrant/puphpet/files/exec-once/usarugbystats-setup.sh
@@ -5,7 +5,7 @@ cp -R /vagrant/files/* /project/config/autoload;
 
 cd /project;
 echo ">>> Installing dependencies via Composer...";
-composer install --dev;
+COMPOSER_PROCESS_TIMEOUT=3600 composer install --dev;
 
 echo ">>> Setting up initial database structures...";
 php ./bin/doctrine-module orm:schema-tool:create
