3fb76ab1fa5042785f629d6fc5f510689f106e60
Adam Lundrigan <adam@lundrigan.ca>
2014-08-07 17:49:42 -0230

    Change post-login dashboard redirects Closes #108

--- module/UsaRugbyStats/CompetitionFrontend/src/Controller/Dashboard/RouterController.php
+++ module/UsaRugbyStats/CompetitionFrontend/src/Controller/Dashboard/RouterController.php
@@ -8,7 +8,7 @@ class RouterController extends AbstractActionController
     public function indexAction()
     {
         $user = $this->zfcUserAuthentication()->getIdentity();
-        $userRoles = $user->getRoles();
+        $userRoles = $user->getRoleAssignments();
         if (count($userRoles) == 0) {
             throw new \RuntimeException('Your account does not have any assigned roles.  Please contact technical support for assistance.');
         }
@@ -16,25 +16,47 @@ class RouterController extends AbstractActionController
         $controllerPattern = 'usarugbystats_competition-frontend_dashboard_%s_controller';
 
         // Display the correct dashboard based on user's role
-        // @TODO Users with multiple roles should be able to choose which one is active
-        $role = array_pop($userRoles);
-        switch ( $role->getName() ) {
-            case 'super_admin':
-                return $this->redirect()->toRoute('zfcadmin');
-            case 'union_admin':
-                $controllerName = sprintf($controllerPattern, 'union-admin');
-                break;
-            case 'competition_admin':
-                $controllerName = sprintf($controllerPattern, 'competition-admin');
-                break;
-            case 'team_admin':
-                $controllerName = sprintf($controllerPattern, 'team-admin');
-                break;
-            case 'member':
-            default:
-                return $this->redirect()->toRoute('usarugbystats_frontend_player', ['id' => $user->getId()]);
+        foreach ($userRoles as $roleAssignment) {
+            $role = $roleAssignment->getRole();
+            switch ( $role->getName() ) {
+                case 'super_admin':
+                    return $this->redirect()->toRoute('zfcadmin');
+                case 'union_admin':
+                    $managedUnions = $roleAssignment->getManagedUnions();
+                    if ( $managedUnions->count() == 1 ) {
+                        return $this->redirect()->toRoute(
+                            'usarugbystats_frontend_union',
+                            ['id' => $managedUnions->current()->getId()]
+                        );
+                    }
+                    $controllerName = sprintf($controllerPattern, 'union-admin');
+
+                    return $this->forward()->dispatch($controllerName);
+                case 'competition_admin':
+                    $managedComps = $roleAssignment->getManagedCompetitions();
+                    if ( $managedComps->count() == 1 ) {
+                        return $this->redirect()->toRoute(
+                            'usarugbystats_frontend_competition',
+                            ['id' => $managedComps->current()->getId()]
+                        );
+                    }
+                    $controllerName = sprintf($controllerPattern, 'competition-admin');
+
+                    return $this->forward()->dispatch($controllerName);
+                case 'team_admin':
+                    $managedTeams = $roleAssignment->getManagedTeams();
+                    if ( $managedTeams->count() == 1 ) {
+                        return $this->redirect()->toRoute(
+                            'usarugbystats_frontend_team',
+                            ['id' => $managedTeams->current()->getId()]
+                        );
+                    }
+                    $controllerName = sprintf($controllerPattern, 'team-admin');
+
+                    return $this->forward()->dispatch($controllerName);
+            }
         }
 
-        return $this->forward()->dispatch($controllerName);
+        return $this->redirect()->toRoute('usarugbystats_frontend_player', ['id' => $user->getId()]);
     }
 }
