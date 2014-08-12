366cda50a42c7729252c350a9749ad7bcc41d0e9
Adam Lundrigan <adam@lundrigan.ca>
2014-08-12 11:27:39 -0230

    If team membership data is provided in a serialized format, explode it and build the division tree

--- module/UsaRugbyStats/Competition/src/DataImporter/ImportCompetitionsTask.php
+++ module/UsaRugbyStats/Competition/src/DataImporter/ImportCompetitionsTask.php
@@ -25,12 +25,54 @@ class ImportCompetitionsTask implements TaskInterface, LoggerAwareInterface
     {
         $this->getLogger()->debug('Importing Competition records...');
 
+        $digitFilter = new \Zend\Filter\Digits();
+
         // Allow manually setting the record identifer
         $metadata = $this->svcCompetition->getObjectManager()->getClassMetadata('UsaRugbyStats\Competition\Entity\Competition');
         $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE);
 
         foreach ($data as $competition) {
             $this->getLogger()->debug(" - {$competition['name']}");
+
+            // Process Team Memberships if provided
+            if ( isset($competition['teams']) && is_scalar($competition['teams']) ) {
+                $competition['divisions'] = array();
+
+                $divisions = explode(';', $competition['teams']);
+                foreach ($divisions as $pairKey => $divisionData) {
+                    if ( strstr($divisionData, '=') !== false ) {
+                        list($divName,$divTeamList) = explode('=', $divisionData);
+                    } else {
+                        $divName = $pairKey;
+                        $divTeamList = $divisionData;
+                    }
+
+                    if ( is_numeric($divName) ) {
+                        $divName = "Division " . ($divName+1);
+                    }
+                    $divTeams = explode(',', $divTeamList);
+                    $teamMemberships = array();
+                    foreach ($divTeams as $teamid) {
+                        $teamid = $digitFilter->filter($teamid);
+                        if ( ! empty($teamid) ) {
+                            array_push($teamMemberships, [
+                                'id' => NULL,
+                                'team' => $teamid
+                            ]);
+                        }
+                    }
+                    if ( empty($teamMemberships) ) {
+                        continue;
+                    }
+
+                    array_push($competition['divisions'], [
+                        'id' => NULL,
+                        'name' => $divName,
+                        'teamMemberships' => $teamMemberships,
+                    ]);
+                }
+            }
+            unset($competition['teams']);
 
             $session = $this->svcCompetition->startSession();
             $session->form = $this->serviceLocator->get('usarugbystats_competition_competition_createform');
