f70ff3a1530cbd5683a4badd86349def06754342
Adam Lundrigan <adam@lundrigan.ca>
2014-08-17 15:31:22 -0230

    EmptyUnionTeamCollectionListener was missed during ServiceExtension implementation (form.bind now happens before data is injected into the form) Fixes #126

--- module/UsaRugbyStats/Competition/src/Listeners/EmptyUnionTeamCollectionListener.php
+++ module/UsaRugbyStats/Competition/src/Listeners/EmptyUnionTeamCollectionListener.php
@@ -13,7 +13,7 @@ class EmptyUnionTeamCollectionListener implements ListenerAggregateInterface
 
     public function attach(EventManagerInterface $events)
     {
-        $this->listeners[] = $events->attach('form.bind', array($this, 'run'), -9999); // pre
+        $this->listeners[] = $events->attach('form.populate', array($this, 'run'), -9999); // pre
     }
 
     public function run(EventInterface $e)
@@ -23,13 +23,21 @@ class EmptyUnionTeamCollectionListener implements ListenerAggregateInterface
 
         // Only apply this hack if the collection is in the validation group
         $vg = $e->getParams()->form->getValidationGroup();
-        if ( $vg != FormInterface::VALIDATE_ALL && !isset($vg['union']['teams']) ) {
+
+        if ( !is_null($vg) && $vg != FormInterface::VALIDATE_ALL && !isset($vg['union']['teams']) ) {
+            return;
+        }
+        if ( empty($data) ) {
             return;
         }
 
         // @HACK to fix GH-15 (Can't empty an existing Collection)
         if ( !isset($data['union']['teams']) || empty($data['union']['teams']) ) {
             $entity->removeTeams($entity->getTeams());
+            $coll = $e->getParams()->form->get('union')->get('teams');
+            foreach ( $coll as $item ) {
+                $coll->remove($item->getName());
+            }
         }
     }
 }
