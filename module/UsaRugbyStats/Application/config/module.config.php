83a9ac1c9fad7011dc73efc2d06cc5a9ae852fc1
Adam Lundrigan <adam@lundrigan.ca>
2014-08-19 10:19:13 -0230

    Make the 403 error page a little more descriptive

--- module/UsaRugbyStats/Application/config/module.config.php
+++ module/UsaRugbyStats/Application/config/module.config.php
@@ -46,6 +46,7 @@ return array(
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/403'               => __DIR__ . '/../view/error/403.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
             'zfc-admin/admin/index'   => __DIR__ . '/../view/zfc-admin/admin/index.phtml',
         ),
