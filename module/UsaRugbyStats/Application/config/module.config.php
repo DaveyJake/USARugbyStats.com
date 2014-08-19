417d2dfd07083f086f1a4cff59db2ab629e4876b
Adam Lundrigan <adam@lundrigan.ca>
2014-08-19 12:01:51 -0230

    Update iframe layout

--- module/UsaRugbyStats/Application/config/module.config.php
+++ module/UsaRugbyStats/Application/config/module.config.php
@@ -45,6 +45,7 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/iframe'           => __DIR__ . '/../view/layout/iframe.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/403'               => __DIR__ . '/../view/error/403.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
