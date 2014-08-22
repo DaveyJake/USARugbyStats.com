a90c5e43fd6a556caec620c6ee5a67c3db93ee91
Adam Lundrigan <adam@lundrigan.ca>
2014-08-22 12:21:25 -0230

    Add CompetitionMatchEventController to CompetitionApi (only implements get, create and delete currently)

--- module/UsaRugbyStats/CompetitionApi/config/module.config.php
+++ module/UsaRugbyStats/CompetitionApi/config/module.config.php
@@ -3,6 +3,7 @@ return array(
     'controllers' => array(
         'invokables' => array(
             'usarugbystats_competition-api_competition_match' => 'UsaRugbyStats\CompetitionApi\Controller\CompetitionMatchController',
+            'usarugbystats_competition-api_competition_match_event' => 'UsaRugbyStats\CompetitionApi\Controller\CompetitionMatchEventController',
             'usarugbystats_competition-api_competition_match_prepare-form' => 'UsaRugbyStats\CompetitionApi\Controller\CompetitionMatchPrepareFormController',
         ),
     ),
@@ -15,11 +16,26 @@ return array(
                     'route'    => '/api/competition/:cid/match[/:id]/prepare-form',
                     'constraints' => array(
                         'cid' => '\d{1,}',
-                        'id' => '\d{0,}',
+                        'id' => '\d{1,}',
                     ),
                     'defaults' => array(
                         'controller' => 'usarugbystats_competition-api_competition_match_prepare-form',
                         'action'     => 'prepare-form',
+                    ),
+                ),
+                'may_terminate' => true,
+            ),
+            'usarugbystats_competition-api_competition_match_events' => array(
+                'type'    => 'Segment',
+                'options' => array(
+                    'route'    => '/api/competition/:cid/match/:mid/event[/:id]',
+                    'constraints' => array(
+                        'cid' => '\d{1,}',
+                        'mid' => '\d{1,}',
+                        'id' => '\d{1,}',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'usarugbystats_competition-api_competition_match_event',
                     ),
                 ),
                 'may_terminate' => true,
@@ -30,7 +46,7 @@ return array(
                     'route'    => '/api/competition/:cid/match[/:id]',
                     'constraints' => array(
                         'cid' => '\d{1,}',
-                        'id' => '\d{0,}',
+                        'id' => '\d{1,}',
                     ),
                     'defaults' => array(
                         'controller' => 'usarugbystats_competition-api_competition_match',
