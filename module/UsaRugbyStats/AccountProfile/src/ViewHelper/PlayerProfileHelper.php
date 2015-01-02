afd2bbd2e7a51bce5111b36b0293c001016a66ba
Adam Lundrigan <adam@lundrigan.ca>
2015-01-02 14:55:15 -0330

    CS fix

--- module/UsaRugbyStats/AccountProfile/src/ViewHelper/PlayerProfileHelper.php
+++ module/UsaRugbyStats/AccountProfile/src/ViewHelper/PlayerProfileHelper.php
@@ -11,15 +11,16 @@ class PlayerProfileHelper extends AbstractHelper
      * @var ProfileService
      */
     protected $service;
-    
+
     public function __construct(ProfileService $svc)
     {
         $this->service = $svc;
     }
-    
+
     public function __invoke(UserInterface $player)
     {
         $obj = $this->service->getProfileForUser($player);
+
         return $obj ?: ($this->service->getFormPrototype()->getObject());
     }
 }
